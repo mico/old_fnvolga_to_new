e6cb0dc6c070fd366f2edcc2137a315e514ffa33
Artur Komarov <artur.komarov@gmail.com>
2019-02-19 22:38:36 +0700

    Migrate article rubric

--- migrate.rb
+++ migrate.rb
@@ -1,14 +1,12 @@
 # В общем, статьи и выпуски газет с него надо присоединить к фн-волге и переформатировать:
 # - скопировать картинки с сайта в каталог фн-волги,
 # - поменять в таблицах ссылки на них
-# - при переносе таблиц, проверять идентичность авторов и рубрик на обоих сайтах, если отличаются, то надо создавать на фн-волге новые, одинаковые - подставлять
 # Выпуски - 208 штук
 # И еще сравнивать порядка 50 рубрик, и авторов десятка два. Но это я вручную могу сделать и создать недостающих
 
 require 'mysql2'
 require 'byebug'
 require 'CSV'
-# Articles to fs_article
 
 @authors_mapping = CSV.read('mapping/authors.csv').to_h
 @category_mapping = CSV.read('mapping/category.csv').to_h
@@ -22,7 +20,9 @@ article_migrate = {
   'FROM_UNIXTIME(Date)': %w[published created modified],
   'Author': 'author_id',
   'Picture': 'logo',
-  'PicTitle': 'logo_caption' }
+  'PicTitle': 'logo_caption',
+  'Category': 'article_rubric_id',
+  'Subcategory': 'article_rubric_id' }
 
 @author_migrate = {
   'FirstName': 'name',
@@ -30,6 +30,10 @@ article_migrate = {
   'JobTitle': 'occupation',
   'Text': 'description',
   'Photo': 'b_image',
+}
+
+@category_migrate = {
+  'Title': 'title'
 }
 
 @client_from = Mysql2::Client.new(host: '127.0.0.1', username: 'root',
@@ -79,21 +83,48 @@ def migrate_author(id)
   end
 end
 
+def migrate_rubric(id, subcategory_id)
+  # use subcategory if available
+  return @subcategory_mapping[subcategory_id.to_s].to_i if @subcategory_mapping.key?(subcategory_id.to_s)
+  return @category_mapping[id.to_s].to_i if @category_mapping.key?(id.to_s)
+  if subcategory_id.empty?
+    res = @client_from.query('SELECT %s FROM Category WHERE id = %d' %
+                             [@category_migrate.keys.join(','), id])
+  else
+    res = @client_from.query('SELECT %s FROM Subcategory WHERE id = %d' %
+                             [@category_migrate.keys.join(','), subcategory_id])
+  end
+  return unless res.any?
+  values = {}
+  values['state'] = 1
+  @category_migrate.map do |k, v|
+    values[v] = row[k.to_s]
+  end
+  q = "INSERT INTO fs_newspaper_article_rubric (`%s`) VALUES ('%s')" %
+      [@category_migrate.values.join('`, `'), values.values.join("', '")]
+  @client_to.query(q)
+  @client_to.last_id
+end
+
 @client_from.query('SELECT %s FROM Articles limit 2' %
                   article_migrate.keys.join(',')).each do |row|
   values = make_migration(article_migrate, row)
   values['state'] = 1
   author_id = migrate_author(row['Author'])
-
   values.delete('author_id')
-  @client_to.query("INSERT INTO fs_article (`%s`) VALUES ('%s')" %
+
+  article_rubric_id = migrate_rubric(row['Category'], row['Subcategory'])
+  values['article_rubric_id'] = article_rubric_id if article_rubric_id
+
+  @client_to.query("INSERT INTO fs_newspaper_article (`%s`) VALUES ('%s')" %
                   [values.keys.join('`, `'), values.values.join("', '")])
   article_id = @client_to.last_id
   puts "article_id: #{article_id}"
   puts "author_id: #{author_id}"
+  puts "rubric_id: #{article_rubric_id}"
 
   if author_id
-    @client_to.query('INSERT INTO fs_article_author (article_id, author_id) VALUES (%d, %d)' %
+    @client_to.query('INSERT INTO fs_newspaper_article_author (newspaper_article_id, author_id) VALUES (%d, %d)' %
                      [article_id, author_id])
   end
 
