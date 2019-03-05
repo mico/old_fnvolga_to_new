f97c8244d92118c2fbfb0f0bb2f13c0ff1ddbe94
Artur Komarov <artur.komarov@gmail.com>
2019-03-05 10:13:35 +0700

    Working stable version

--- lib/migrate.rb
+++ lib/migrate.rb
@@ -3,9 +3,9 @@ require 'byebug'
 require 'CSV'
 require 'yaml'
 
-@mappings = {'issue': {}}
+@mappings = { 'issue' => {} }
 Dir['mapping/*csv'].each do |mapping|
-  name = migration.sub('.csv', '').sub('mapping/', '')
+  name = mapping.sub('.csv', '').sub('mapping/', '')
   @mappings[name] = CSV.read(mapping).to_h
 end
 
@@ -46,22 +46,30 @@ def migration_row(mapping, migrate_map, row_from)
     return result.first['id'] if result.any?
   end
 
-  values = fields.map { |key, value| [value.to_sym, row_from[key.to_s]] }.to_h
+  values = {}
+  fields = migrate_map['fields']
+  fields.each do |key, value|
+    if value.is_a?(Array)
+      value.each { |v| values[v.to_sym] = row_from[key.to_s] }
+    else
+      values[value.to_sym] = row_from[key.to_s]
+    end
+  end
   migrate_map['custom_fields'].each do |field, code|
     values[field.to_sym] = code.is_a?(String) && code.gsub(/\#\{(.*?)\}/) { eval($1) } || code
   end
 
-  relations = migrate_map[entity]['relations']
+  relations = migrate_map['relations']
 
   # one to many relations
   if relations
     relations.each do |relation, params|
       next unless params['type'] == 'onetomany'
-      relation_id = row[relation]
+      relation_id = row_from[relation]
       next unless relation_id
       # skip already found relations (if duplicates in migration description)
       next if values.key?(fields[relation])
-      values[fields[relation]] = migrate_entity(relation.downcase, relation_id)
+      values[fields[relation].to_sym] = migrate_entity(relation.downcase, relation_id)
     end
   end
 
@@ -69,7 +77,12 @@ def migration_row(mapping, migrate_map, row_from)
                  table_to: migrate_to,
                  keys: values.keys.join('`, `'),
                  values: values.values.join("', '"))
-  puts query
+  query_print = format("INSERT INTO %<table_to>s (`%<keys>s`) VALUES ('%<values>s')",
+                 table_to: migrate_to,
+                 keys: values.keys.join('`, `'),
+                 values: values.values.map{ |v| v.is_a?(String) && v.truncate || v }.join("', '"))
+
+  puts query_print
   @client_to.query(query)
   last_id = @client_to.last_id
 
@@ -88,18 +101,20 @@ def migration_row(mapping, migrate_map, row_from)
   end
 
   # many to many relations
-  manytomany_relations.each do |relation, params|
-    # manytomany
-    entity_id = migrate_entity(relation.downcase, row[relation])
-    next unless entity_id
-    @client_to.query(
-      format('INSERT INTO %<table>s (%<foreign_id_name>s, %<primary_id_name>s) VALUES (%<foreign_id>d, %<primary_id>d)',
+  if relations
+    manytomany_relations(relations).each do |relation, params|
+      # manytomany
+      entity_id = migrate_entity(relation.downcase, row_from[relation])
+      next unless entity_id
+      query = format('INSERT INTO %<table>s (%<foreign_id_name>s, %<primary_id_name>s) VALUES (%<foreign_id>d, %<primary_id>d)',
               table: params['table'],
               foreign_id_name: params['foreign_id'],
-              primary_id_name: params['primary_id'],
+              primary_id_name: params['primary_key'],
               foreign_id: last_id,
               primary_id: entity_id)
-    )
+      puts query
+      @client_to.query(query)
+    end
   end
 
   last_id
@@ -107,7 +122,7 @@ end
 
 def migration(mapping, migrate_map, id)
   return mapping[id] if mapping.key?(id)
-  fields = get_fields(@migration[entity])
+  fields = get_fields(migrate_map)
   res = @client_from.query(
     format('SELECT %<keys>s FROM `%<table>s` WHERE id = %<id>d',
            keys: fields.keys.join(','),
@@ -138,12 +153,12 @@ def manytomany_relations(relations)
 end
 
 def get_fields(migrations)
-  fields = migrations[entity]['fields']
+  fields = migrations['fields'].dup
   # TODO: add manytomany relations to fields
   # if relations.has_manytomany?
   #   fields << relations.manytomany.fields
-  relations = migrations[entity]['relations']
-  fields += manytomany_relations(relations).map { |relation, _| relation }
+  relations = migrations['relations']
+  fields.merge!(manytomany_relations(relations).map { |relation, _| [relation, nil] }.to_h) if relations
   fields
 end
 
@@ -154,6 +169,6 @@ def run(entity = 'article')
                  table: migrations['from'])
   puts query
   @client_from.query(query).each do |row|
-    migration_row(@mapping[entity], migrations, row)
+    migration_row(@mappings[entity], migrations, row)
   end
 end
